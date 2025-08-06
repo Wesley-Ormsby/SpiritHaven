e55ab5ae4435aa3ea192ce4a33df1dc259c6c92c
Wesley Ormsby <wesley_ormsby@outlook.com>
2025-08-06 08:05:36 -0400

    feat: image preview dialog loading state
    - Added loading state to image preview dialog

--- src/components/ImageDialog.vue
+++ src/components/ImageDialog.vue
@@ -1,35 +1,43 @@
 <script lang="ts" setup>
 import Dialog from 'primevue/dialog'
 import { X } from 'lucide-vue-next'
-const {src} = defineProps<{
+import { ref } from 'vue'
+import Loader from './Loader.vue'
+const { src } = defineProps<{
   src: string
 }>()
 const visible = defineModel<boolean>('visible')
+
+const loaded = ref(false)
 </script>
 <template>
-    <Dialog
-      v-model:visible="visible"
-      modal
-      pt:root:style="border:0px transparent;background-color:transparent;box-shadow:none;"
-      dismissableMask
-      :draggable="false"
-    >
-      <template #container="{ closeCallback }">
-        <div class="image-preview-dialog">
-          <img class="dialog-img" :src alt="Preview image"/>
-          <div @click="closeCallback" class="close-button">
-            <X></X>
-          </div>
+  <Dialog
+    v-model:visible="visible"
+    modal
+    pt:root:style="border:0px transparent;background-color:transparent;box-shadow:none;"
+    dismissableMask
+    :draggable="false"
+    @show="loaded = false"
+  >
+    <template #container="{ closeCallback }">
+      <div class="image-preview-dialog">
+        <img class="dialog-img" v-show="loaded" :src alt="Preview image" @load="loaded = true" />
+        <div v-if="!loaded" class="loader-container">
+          <Loader></Loader>
         </div>
-      </template>
-    </Dialog>
+        <div @click="closeCallback" class="close-button">
+          <X></X>
+        </div>
+      </div>
+    </template>
+  </Dialog>
 </template>
 
 <style lang="css" scoped>
 .dialog-img {
-    max-width: 90vw;
-    max-height: 90vh;
-    border-radius: 10px;
+  max-width: 90vw;
+  max-height: 90vh;
+  border-radius: 10px;
 }
 .image-preview-dialog {
   position: relative;
@@ -53,15 +61,22 @@ const visible = defineModel<boolean>('visible')
   color: var(--p-zinc-100);
   background-color: rgba(0, 0, 0, 0.7);
 }
+.loader-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30vw;
+  height: 50vh;
+}
 @media only screen and (max-width: 500px) {
-   .close-button {
-        padding: 8px;
-        right: 5px;
-  top: 5px;
-    }
-    .close-button svg {
-        width:15px;
-        height: 15px;
-    }
+  .close-button {
+    padding: 8px;
+    right: 5px;
+    top: 5px;
+  }
+  .close-button svg {
+    width: 15px;
+    height: 15px;
+  }
 }
 </style>
